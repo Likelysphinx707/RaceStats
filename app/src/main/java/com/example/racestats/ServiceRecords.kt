513bcd684bf07765591eb66c9017c136b3edfd5f
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2023-05-05 00:15:44 -0600

    Polishing add feature for Maintenance Records

--- app/src/main/java/com/example/racestats/ServiceRecords.kt
+++ app/src/main/java/com/example/racestats/ServiceRecords.kt
@@ -32,68 +32,14 @@ class ServiceRecords : AppCompatActivity() {
         newRecordLayout = findViewById(R.id.new_record_layout)
 
         // set onClickListener on the button
-//        addButton.setOnClickListener {
-//            // toggle the visibility of the new record layout
-//            if (newRecordLayout.visibility == View.GONE) {
-//                newRecordLayout.visibility = View.VISIBLE
-//            } else {
-//                // create a new LinearLayout to represent the record
-//                val recordLayout = LinearLayout(this)
-//                recordLayout.orientation = LinearLayout.HORIZONTAL
-//                recordLayout.layoutParams = LinearLayout.LayoutParams(
-//                    LinearLayout.LayoutParams.MATCH_PARENT,
-//                    LinearLayout.LayoutParams.WRAP_CONTENT
-//                )
-//
-//                // create three TextViews to display the service, mileage, and date
-//                val serviceTextView = TextView(this)
-//                serviceTextView.text = serviceEditText.text
-//                serviceTextView.setTextColor(resources.getColor(R.color.white))
-//                serviceTextView.textSize = 18f
-//                serviceTextView.layoutParams = LinearLayout.LayoutParams(
-//                    0,
-//                    LinearLayout.LayoutParams.WRAP_CONTENT,
-//                    1F
-//                )
-//                serviceTextView.gravity = Gravity.CENTER // set text alignment to center
-//
-//                val mileageTextView = TextView(this)
-//                mileageTextView.text = mileageEditText.text
-//                mileageTextView.setTextColor(resources.getColor(R.color.white))
-//                mileageTextView.textSize = 18f
-//                mileageTextView.layoutParams = LinearLayout.LayoutParams(
-//                    0,
-//                    LinearLayout.LayoutParams.WRAP_CONTENT,
-//                    1F
-//                )
-//                mileageTextView.gravity = Gravity.CENTER // set text alignment to center
-//
-//                val dateTextView = TextView(this)
-//                dateTextView.text = dateEditText.text
-//                dateTextView.setTextColor(resources.getColor(R.color.white))
-//                dateTextView.textSize = 18f
-//                dateTextView.layoutParams = LinearLayout.LayoutParams(
-//                    0,
-//                    LinearLayout.LayoutParams.WRAP_CONTENT,
-//                    1F
-//                )
-//                dateTextView.gravity = Gravity.CENTER // set text alignment to center
-//
-//                // add the TextViews to the record LinearLayout
-//                recordLayout.addView(serviceTextView)
-//                recordLayout.addView(mileageTextView)
-//                recordLayout.addView(dateTextView)
-//
-//                // add the record LinearLayout to the service_records_list LinearLayout
-//                serviceRecordsList.addView(recordLayout)
-//            }
-//        }
-
         addButton.setOnClickListener {
             // Toggle the visibility of the new record layout
             if (newRecordLayout.visibility == View.GONE) {
                 newRecordLayout.visibility = View.VISIBLE
             } else {
+                // Change the addButtons text from add New to save
+                addButton.text = "Save"
+
                 // Create a new LinearLayout to represent the record
                 val recordLayout = LinearLayout(this)
                 recordLayout.orientation = LinearLayout.HORIZONTAL
@@ -106,7 +52,7 @@ class ServiceRecords : AppCompatActivity() {
                 val serviceTextView = TextView(this)
                 serviceTextView.text = serviceEditText.text
                 serviceTextView.setTextColor(resources.getColor(R.color.white))
-                serviceTextView.textSize = 18f
+                serviceTextView.textSize = 22f
                 serviceTextView.layoutParams = LinearLayout.LayoutParams(
                     0,
                     LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -117,7 +63,7 @@ class ServiceRecords : AppCompatActivity() {
                 val mileageTextView = TextView(this)
                 mileageTextView.text = mileageEditText.text
                 mileageTextView.setTextColor(resources.getColor(R.color.white))
-                mileageTextView.textSize = 18f
+                mileageTextView.textSize = 22f
                 mileageTextView.layoutParams = LinearLayout.LayoutParams(
                     0,
                     LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -128,7 +74,7 @@ class ServiceRecords : AppCompatActivity() {
                 val dateTextView = TextView(this)
                 dateTextView.text = dateEditText.text
                 dateTextView.setTextColor(resources.getColor(R.color.white))
-                dateTextView.textSize = 18f
+                dateTextView.textSize = 22f
                 dateTextView.layoutParams = LinearLayout.LayoutParams(
                     0,
                     LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -142,6 +88,9 @@ class ServiceRecords : AppCompatActivity() {
                     recordLayout.addView(serviceTextView)
                     recordLayout.addView(mileageTextView)
                     recordLayout.addView(dateTextView)
+
+                    // Let the user know that the record was added successfully
+                    Toast.makeText(this, "Record Added Successfully!", Toast.LENGTH_LONG).show();
                 } else {
                     // Show a message to the user indicating that all three fields are required
                     Toast.makeText(this, "Please fill in at least one field to create a new record", Toast.LENGTH_LONG).show();
@@ -150,7 +99,9 @@ class ServiceRecords : AppCompatActivity() {
                 // Add the record LinearLayout to the service_records_list LinearLayout
                 serviceRecordsList.addView(recordLayout)
 
-                // Reset the EditText fields and hide the new record layout
+                // Reset the EditText fields, hide the new record layout and set "Save" back to "Add New"
+                addButton.text = "Add New"
+
                 serviceEditText.text.clear()
                 mileageEditText.text.clear()
                 dateEditText.text.clear()
