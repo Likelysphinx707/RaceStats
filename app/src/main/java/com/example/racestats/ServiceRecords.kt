b607e583fbdf34732731f71448f7f69d0eedf5d4
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2023-05-20 02:01:09 -0600

    Added base for an X button that would allow users to delete recordes. Not working properly

--- app/src/main/java/com/example/racestats/ServiceRecords.kt
+++ app/src/main/java/com/example/racestats/ServiceRecords.kt
@@ -7,6 +7,7 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class ServiceRecords : AppCompatActivity() {
+    // Declare private properties for the buttons, layouts, and EditTexts
     private lateinit var addButton: Button
     private lateinit var editButton: Button
     private lateinit var deleteButton: Button
@@ -14,10 +15,15 @@ class ServiceRecords : AppCompatActivity() {
     private lateinit var serviceEditText: EditText
     private lateinit var mileageEditText: EditText
     private lateinit var dateEditText: EditText
+    private lateinit var recordViews: MutableList<View>
+    private lateinit var deleteButtonX: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service_records)
+
+        // Initialize the recordViews list
+        recordViews = mutableListOf()
 
         // find the service_records_list LinearLayout
         val serviceRecordsList = findViewById<LinearLayout>(R.id.service_records_list)
@@ -26,6 +32,7 @@ class ServiceRecords : AppCompatActivity() {
         addButton = findViewById(R.id.add_new_button)
         editButton = findViewById(R.id.edit_button)
         deleteButton = findViewById(R.id.delete_button)
+        deleteButtonX = findViewById(R.id.delete_button_x)
 
         // find the EditTexts
         serviceEditText = findViewById(R.id.service_edittext)
@@ -99,22 +106,45 @@ class ServiceRecords : AppCompatActivity() {
                 )
                 dateTextView.gravity = Gravity.CENTER // set text alignment to center
 
+                val deleteButtonX = Button(this)
+                deleteButtonX.text = "x"
+                deleteButtonX.setTextColor(resources.getColor(R.color.white))
+                deleteButtonX.textSize = 22f
+                deleteButtonX.visibility = View.GONE
+                deleteButtonX.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                deleteButtonX.setOnClickListener {
+                    // Remove the record LinearLayout from the service_records_list LinearLayout
+                    serviceRecordsList.removeView(recordLayout)
+                    // Remove the record view from the recordViews list
+                    recordViews.remove(recordLayout)
+                }
+                deleteButtonX.gravity = Gravity.CENTER // set text alignment to center
+
                 // Check if all three EditText fields have values
                 if (serviceEditText.text.isNotEmpty() || mileageEditText.text.isNotEmpty() || dateEditText.text.isNotEmpty()) {
                     // Add the TextViews to the record LinearLayout
                     recordLayout.addView(serviceTextView)
                     recordLayout.addView(mileageTextView)
                     recordLayout.addView(dateTextView)
+                    recordLayout.addView(deleteButtonX)
 
                     // Let the user know that the record was added successfully
                     Toast.makeText(this, "Record Added Successfully!", Toast.LENGTH_LONG).show();
                 } else {
                     // Show a message to the user indicating that all three fields are required
-                    Toast.makeText(this, "Please fill in at least one field to create a new record", Toast.LENGTH_LONG).show();
+                    Toast.makeText(
+                        this,
+                        "Please fill in at least one field to create a new record",
+                        Toast.LENGTH_LONG
+                    ).show();
                 }
 
                 // Add the record LinearLayout to the service_records_list LinearLayout
                 serviceRecordsList.addView(recordLayout)
+                recordViews.add(recordLayout)
 
                 // Reset the EditText fields, hide the new record layout and set "Save" back to "Add New"
                 addButton.text = "Add New"
@@ -131,5 +161,20 @@ class ServiceRecords : AppCompatActivity() {
                 deleteButton.visibility = View.VISIBLE
             }
         }
+
+        deleteButton.setOnClickListener {
+            if (deleteButtonX.visibility == View.GONE) {
+                // Show all delete buttons
+                for (recordView in recordViews) {
+                    deleteButtonX.visibility = View.VISIBLE
+                }
+            } else {
+                // Hide delete buttons for all records
+                for (recordView in recordViews) {
+                    deleteButtonX.visibility = View.GONE
+                }
+            }
+        }
+
     }
 }
