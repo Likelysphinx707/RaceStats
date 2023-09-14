734fc64d4b8b3c716a9fb615921e962a41b5a130
Alexander Murdock <77793000+Amurdock6@users.noreply.github.com>
2023-09-14 15:08:46 -0600

    Got new gauge layout added as well as a flashing affect

--- app/src/main/java/com/example/racestats/Settings.java
+++ app/src/main/java/com/example/racestats/Settings.java
@@ -1,6 +1,10 @@
 package com.example.racestats;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -10,34 +14,101 @@ public class Settings extends AppCompatActivity {
     private ProgressBar coolantTemperatureGauge;
     private TextView coolantTemperatureText;
 
+    private TextView coolantTempTextSimple;
+    private ImageView coolantLogoSimple;
+
+    private Handler handler = new Handler();
+    private boolean isFlashing = false;
+    private int flashCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.gaugetest);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gaugetest);
+
+    // Set the coolant temperature (0 to 126, adjust as needed)
+    int coolantTemperature = 106; // Change this temperature value
 
         coolantTemperatureGauge = findViewById(R.id.coolantTemperatureGauge);
         coolantTemperatureText = findViewById(R.id.coolantTemperatureText);
 
-        // Set the coolant temperature (0 to 126, adjust as needed)
-        int coolantTemperature = 96; // Change this temperature value
+        coolantLogoSimple = findViewById(R.id.coolantLogoSimple);
+        coolantTempTextSimple = findViewById(R.id.textTempSimple);
+
 
         // Update the temperature text
         coolantTemperatureText.setText(coolantTemperature + "°C");
+        coolantTempTextSimple.setText(coolantTemperature + "°C");
 
-        // Update the gauge color based on temperature
-        updateGaugeColor(coolantTemperature);
-    }
+    // Update the gauge color based on temperature
+    updateGaugeColor(coolantTemperature);
+}
 
     // Method to update the gauge color based on temperature
     private void updateGaugeColor(int temperature) {
         if (temperature > 105) {
             coolantTemperatureGauge.setProgressDrawable(getResources().getDrawable(R.drawable.horizontal_gauge_high));
-        } else if(temperature > 96) {
+
+            // Start the flashing effect if it's not already running
+            if (!isFlashing) {
+                startFlashingEffect();
+            }
+
+            coolantLogoSimple.setImageResource(R.drawable.coolant_logo_red);
+            coolantTempTextSimple.setTextColor(Color.parseColor("#ff0000"));
+        } else if (temperature > 96) {
             coolantTemperatureGauge.setProgressDrawable(getResources().getDrawable(R.drawable.horizontal_gauge_medium));
+
+            // Stop the flashing effect if it's running
+            stopFlashingEffect();
+
+            coolantTempTextSimple.setTextColor(Color.parseColor("#ffe222"));
+            coolantLogoSimple.setImageResource(R.drawable.coolant_logo_yellow);
         } else {
             coolantTemperatureGauge.setProgressDrawable(getResources().getDrawable(R.drawable.horizontal_gauge));
+
+            // Stop the flashing effect if it's running
+            stopFlashingEffect();
+
+            coolantTempTextSimple.setTextColor(Color.parseColor("#FFFFFF"));
+            coolantLogoSimple.setImageResource(R.drawable.coolant_logo_white);
         }
 
         coolantTemperatureGauge.setProgress(temperature);
+    }
+
+    private void startFlashingEffect() {
+        isFlashing = true;
+        flashCount = 0;
+        handler.postDelayed(flashingRunnable, 500); // Start flashing every 500ms
+    }
+
+    private void stopFlashingEffect() {
+        isFlashing = false;
+        handler.removeCallbacks(flashingRunnable); // Stop the flashing effect
+    }
+
+    private Runnable flashingRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (flashCount < 8) { // 8 times to flash (4 times on, 4 times off)
+                if (coolantLogoSimple.getVisibility() == View.VISIBLE) {
+                    coolantLogoSimple.setVisibility(View.INVISIBLE);
+                } else {
+                    coolantLogoSimple.setVisibility(View.VISIBLE);
+                }
+                flashCount++;
+                handler.postDelayed(this, 500); // Repeat the flashing every 500ms
+            } else {
+                coolantLogoSimple.setVisibility(View.VISIBLE); // Ensure it's visible when done flashing
+                isFlashing = false;
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopFlashingEffect(); // Stop the flashing effect when the activity is destroyed
     }
 }
